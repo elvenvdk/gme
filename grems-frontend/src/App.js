dcd02c01b47560ab264a71a2de12b3c3eb7b2b88
Alvin van der Kuech <alvin.vandekuech@bjss.com>
2020-02-26 21:00:03 -0500

    Added landing page scaffolding and routing

--- grems-frontend/src/App.js
+++ grems-frontend/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import LandingPage from './components/landing/Landing';
 
 const App = () => {
-  return <div className='App'>Grandma Emma's</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact component={LandingPage} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
