2695c2591db31c9c1c1dbf07f1f153d36c8387c9
Alvin van der Kuech <alvin.vandekuech@bjss.com>
2020-05-19 22:37:36 -0400

    Cleaned up code base

--- grems-frontend/src/App.js
+++ grems-frontend/src/App.js
@@ -3,18 +3,20 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import PrivateRoute from './auth/PrivateRoute';
 
-import HomePage from './components/home/Home';
+import HomePage from './components/Home';
 import Signup from './components/auth/Signup';
 import Signin from './components/auth/Signin';
-import Shop from './components/shop/Shop';
+import Shop from './components/Shop';
 import Checkout from './components/checkout/Checkout';
 
-import Nav from './components/common/navbar/Nav';
+import PageHeader from './components/PageHeader';
+import Nav from './components/common/Nav';
 
 const App = () => {
   return (
     <Router classname='app'>
-      <Nav classname='app__nav' />
+      <PageHeader />
+      {/* <Nav classname='app__nav' /> */}
       <Switch>
         <Route path='/' exact component={HomePage} />
         <Route path='/signup' exact component={Signup} />
---
+++ grems-frontend/src/components/PageHeader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PageHeader = () => {
+  return <div>Page header...</div>;
+};
+
+export default PageHeader;
