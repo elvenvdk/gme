92ef7c02f353f7c91c5286415c99c3f68c257a35
Alvin van der Kuech <alvin.vandekuech@bjss.com>
2020-05-23 14:01:56 -0400

    Added pageHeader

---
+++ grems-frontend/src/components/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import PrivateRoute from '../auth/PrivateRoute';
+
+import HomePage from './Home';
+// import Signup from '../auth/Signup';
+// import Signin from '../auth/Signin';
+import Shop from './Shop';
+import Checkout from './checkout/Checkout';
+
+import PageHeader from './pageHeader/PageHeader';
+import Nav from './common/Nav';
+
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Router>
+        <PageHeader />
+        {/* <Nav classname='app__nav' /> */}
+        <Switch>
+          <Route path='/' exact component={HomePage} />
+          {/* <Route path='/signup' exact component={Signup} /> */}
+          {/* <Route path='/signin' exact component={Signin} /> */}
+          {/* <Route path='/shop' exact component={Shop} /> */}
+          {/* <PrivateRoute path='/checkout' exact component={Checkout} /> */}
+          <Route path='/checkout' exact component={Checkout} />
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
