4597775e25f09da681b8c3f5c7f636f207ffa435
Alvin van der Kuech <alvin.vandekuech@bjss.com>
2020-05-25 15:15:34 -0400

    Added copywrite

--- grems-frontend/src/components/common/footer/Footer.js
+++ grems-frontend/src/components/common/footer/Footer.js
@@ -7,27 +7,32 @@ import './Footer.scss';
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <h4 className='footer-header'>CONNECT WITH US</h4>
-      <div className='footer-divider'>
-        <div className='footer-divider-dot' />
-        <div className='footer-divider-dot' />
-        <div className='footer-divider-dot' />
-        <div className='footer-divider-dot' />
-        <div className='footer-divider-dot' />
+    <>
+      <div className='footer'>
+        <h4 className='footer-header'>CONNECT WITH US</h4>
+        <div className='footer-divider'>
+          <div className='footer-divider-dot' />
+          <div className='footer-divider-dot' />
+          <div className='footer-divider-dot' />
+          <div className='footer-divider-dot' />
+          <div className='footer-divider-dot' />
+        </div>
+        <div className='footer-link-icons'>
+          <a className='footer-fb-link' href=''>
+            <img className='footer-fb-icon' src={fbIcon} alt='Facebook Icon' />
+          </a>
+          <a className='footer-ig-link' href=''>
+            <img className='footer-ig-icon' src={igIcon} alt='Instagram Icon' />
+          </a>
+        </div>
+        <p className='footer-customer-service'>
+          email us : CustomerService@GrandmaEmmas.com
+        </p>
       </div>
-      <div className='footer-link-icons'>
-        <a className='footer-fb-link' href=''>
-          <img className='footer-fb-icon' src={fbIcon} alt='Facebook Icon' />
-        </a>
-        <a className='footer-ig-link' href=''>
-          <img className='footer-ig-icon' src={igIcon} alt='Instagram Icon' />
-        </a>
-      </div>
-      <p className='footer-customer-service'>
-        email us : CustomerService@GrandmaEmmas.com
+      <p className='copywrite'>
+        Copyright 2020. All Rights Reserved by Grandma.
       </p>
-    </div>
+    </>
   );
 };
 
