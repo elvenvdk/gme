d2042f8e39f530418bb1f0d54307105693c6ebc6
Alvin van der Kuech <alvin.vandekuech@bjss.com>
2020-08-30 12:39:03 -0400

    Created testimionial controller method and route

---
+++ ge-s-api/controllers/testimonials.js
@@ -0,0 +1,40 @@
+const Testimonial = require('../models/testimonials');
+
+exports.createTestimonial = async (req, res) => {
+  const {
+    isCustomer,
+    customer,
+    call,
+    eventDate,
+    eventType,
+    firstName,
+    lastName,
+    email,
+    mobile,
+    facebook,
+    instagram,
+    twitter,
+    testimony,
+  } = req.body;
+  try {
+    const testimonial = await new Testimonial({
+      isCustomer,
+      customer,
+      call,
+      eventDate,
+      eventType,
+      firstName,
+      lastName,
+      email,
+      mobile,
+      [socialMedia.facebook]: facebook,
+      [socialMedia.instragram]: instagram,
+      [socialMedia.twitter]: twitter,
+      testimony,
+    });
+    await testimonial.save();
+    res.send({ msg: 'Testimonial successfully saved' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
---
+++ ge-s-api/routes/testimionials.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+/**
+ * @route create testimonial
+ * @access public
+ */
+
+router.post('/create');
+
+module.exports = router;
