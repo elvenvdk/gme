520f641c67ec7759110448896dd4843f68ab2f4f
Alvin van der Kuech <alvin.vandekuech@bjss.com>
2020-09-21 16:13:30 -0400

    modifid mailer and transport

--- ge-m-svc/transport.js
+++ ge-m-svc/transport.js
@@ -2,8 +2,8 @@ const nodemailer = require('nodemailer');
 
 const transport = nodemailer.createTransport({
   host: process.env.GOOGLE_HOST,
-  port: process.env.TLS_PORT,
-  secure: false,
+  port: process.env.SSL_PORT,
+  secure: true,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASSWORD,
